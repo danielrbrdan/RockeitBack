051817df1e0077dd02958967b20eadd3a96c864e
Daniel Ramos <danielrrbhmg@gmail.com>
2023-07-03 10:24:34 -0300

    Initial Commit

---
+++ src/main/java/com/challenge/conexa/security/JWTAutenticationFilter.java
@@ -0,0 +1,86 @@
+package com.challenge.conexa.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.challenge.conexa.data.UserDetailData;
+import com.challenge.conexa.models.entity.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class JWTAutenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    public static final int TOKEN_EXPIRATION = 9000_000;
+    public static final String TOKEN_PASS = "ecd7e908-0046-49d7-a5ff-1542d8548345";
+
+    private final AuthenticationManager authenticationManager;
+
+    public JWTAutenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+        try {
+            User user = new ObjectMapper()
+                    .readValue(request.getInputStream(), User.class);
+
+            return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    user.getLogin(),
+                    user.getPassword(),
+                    new ArrayList<>()
+            ));
+
+        } catch (IOException e) {
+            throw new RuntimeException("Falha ao autenticar user", e);
+        }
+
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+
+        UserDetailData userData = (UserDetailData) authResult.getPrincipal();
+
+        String token = JWT.create().
+                withSubject(userData.getUsername())
+                //.withExpiresAt(new Date(System.currentTimeMillis() + TOKEN_EXPIRATION))
+                .sign(Algorithm.HMAC512(TOKEN_PASS));
+
+        response.getWriter().write(token);
+        response.getWriter().flush();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/challenge/conexa/security/JWTConfiguration.java
@@ -0,0 +1,70 @@
+package com.challenge.conexa.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.challenge.conexa.service.UserDetailServiceImpl;
+
+
+@EnableWebSecurity
+public class JWTConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailServiceImpl userService;
+    private final PasswordEncoder passwordEncoder;
+
+    public JWTConfiguration(UserDetailServiceImpl userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/user").permitAll()
+
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAutenticationFilter(authenticationManager()))
+                .addFilter(new JWTValidationFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
+        corsConfiguration.addAllowedMethod(HttpMethod.PUT);
+        
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
